3318c2c76438b4f8f479378e2923638dd51cf0b7
Madhuri Naik <37347515+madhurinaik3101@users.noreply.github.com>
2025-08-08 18:08:10 -0400

    Update SpringbootFirstwebappApplicationTests.java

--- springboot-firstwebapp/src/test/java/com/springboot/web/springbootfirstwebapp/SpringbootFirstwebappApplicationTests.java
+++ springboot-firstwebapp/src/test/java/com/springboot/web/springbootfirstwebapp/SpringbootFirstwebappApplicationTests.java
@@ -3,9 +3,11 @@ package com.springboot.web.springbootfirstwebapp;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringRunner.class)
+// import org.springframework.test.context.junit4.SpringRunner;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SpringbootFirstwebappApplicationTests {
 
